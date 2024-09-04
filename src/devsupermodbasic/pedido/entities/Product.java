48af1c64dc8344a3585b36e7abf13df04364d25d
Edvaldo Ferreira <ed.ferreiraoliveira@gmail.com>
2024-09-04 09:51:00 +0100

    Implementação Classe Produto

---
+++ src/devsupermodbasic/pedido/entities/Product.java
@@ -0,0 +1,30 @@
+package devsupermodbasic.pedido.entities;
+
+public class Product {
+    private String name;
+    private double prece;
+
+    public Product(){
+
+    }
+    public Product(String name, double prece){
+        this.name = name;
+        this.prece = prece;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrece() {
+        return prece;
+    }
+
+    public void setPrece(double prece) {
+        this.prece = prece;
+    }
+}
