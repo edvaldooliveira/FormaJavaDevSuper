7397494e9e37ee8b776b389ff5718c9ac825fe5d
Edvaldo Ferreira <ed.ferreiraoliveira@gmail.com>
2024-09-06 16:01:16 +0100

    Polimorfismo Entidades implementadas

---
+++ src/com/devsupermodbasic/exresolvidopolimorfismo/entities/OutsourcedEmployee.java
@@ -0,0 +1,28 @@
+package com.devsupermodbasic.exresolvidopolimorfismo.entities;
+
+public class OutsourcedEmployee extends Employee {
+
+    private Double additionalCharge; //cobrança adicional
+
+    public OutsourcedEmployee(){
+        super();
+    }
+
+    public OutsourcedEmployee(String name, Integer hours, Double valuePerHour, Double additionalCharge) {
+        super(name, hours, valuePerHour);
+        this.additionalCharge = additionalCharge;
+    }
+
+    public Double getAdditionalCharge() {
+        return additionalCharge;
+    }
+
+    public void setAdditionalCharge(Double additionalCharge) {
+        this.additionalCharge = additionalCharge;
+    }
+
+    @Override
+    public double payment(){
+        return super.payment() + additionalCharge * 1.1;
+    }
+}
