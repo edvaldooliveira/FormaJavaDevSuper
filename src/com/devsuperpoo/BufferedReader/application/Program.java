8772b1fca4ea632a154434d3de4d593b96c33aa3
Edvaldo Ferreira <ed.ferreiraoliveira@gmail.com>
2024-09-14 17:12:32 +0100

    FileReader e BufferedReader com melhoria

--- src/com/devsuperpoo/BufferedReader/application/Program.java
+++ src/com/devsuperpoo/BufferedReader/application/Program.java
@@ -8,15 +8,9 @@ public class Program {
     public static void main(String[] args) {
 
         String path = "C:\\temp\\in.txt";
-        FileReader fr = null;
-        BufferedReader br = null;
-
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 
             String line = br.readLine();
-
             while (line != null){
                 System.out.println(line);
                line = br.readLine();
@@ -24,20 +18,6 @@ public class Program {
         }
         catch (IOException e){
             System.out.println("error" + e.getMessage());
-
-        }
-        finally {
-            try {
-                if (fr != null){
-                    fr.close();
-                }
-                if (br != null){
-                    br.close();
-                }
-            }
-            catch (IOException e){
-                System.out.println(e.getMessage());
-            }
         }
     }
 }
