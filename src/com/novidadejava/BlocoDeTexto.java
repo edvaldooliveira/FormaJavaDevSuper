9aacad2bd4881c021a8781dfb4c31ffb44b12be4
Edvaldo Ferreira <ed.ferreiraoliveira@gmail.com>
2024-09-17 13:51:07 +0100

    Bloco de texto

---
+++ src/com/novidadejava/BlocoDeTexto.java
@@ -0,0 +1,22 @@
+package com.novidadejava;
+
+public class BlocoDeTexto {
+    public static void main(String[] args) {
+
+        String listaDeDados = """
+                                
+                                Computador
+                                Rede de computadores
+                                Servidor
+                                Impressora
+                                Scanner
+                                Monitor
+                                Teclado
+                                Mouse
+                                Notebook
+                                
+                              """;
+        System.out.println(listaDeDados);
+
+    }
+}
