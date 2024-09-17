b1e477efc048fe18828aca86ae5c19d2b7d70c12
Edvaldo Ferreira <ed.ferreiraoliveira@gmail.com>
2024-09-17 09:12:08 +0100

    Ordenar numeros

---
+++ src/com/novidadejava/OrdenarNumeros.java
@@ -0,0 +1,19 @@
+package com.novidadejava;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenarNumeros {
+    public static void main(String[] args) {
+
+        List<Integer> numeros = new ArrayList<>(Arrays.asList(10,7,3,15,3,1,9,4,12,5));
+        System.out.println("\n Antes e Depois da ordenação:");
+        System.out.println(" Ordenação tradicional = " + numeros);
+        Collections.sort(numeros, (a,b) -> a.compareTo(b) );
+        System.out.println(" Ordenação com lambda  = " + numeros);
+        
+
+    }
+}
