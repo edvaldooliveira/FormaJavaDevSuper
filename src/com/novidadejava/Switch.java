4474d3bda78165e555193c721a3de061a89b5792
Edvaldo Ferreira <ed.ferreiraoliveira@gmail.com>
2024-09-17 12:23:44 +0100

    Switch case default

--- src/com/novidadejava/Switch.java
+++ src/com/novidadejava/Switch.java
@@ -3,14 +3,18 @@ package com.novidadejava;
 public class Switch {
     public static void main(String[] args) {
 
-        var produto = "Farinhas";
+        var produto = "Edvaldo";
+
         System.out.println();
         switch(produto){
-        case "Azeite"   -> System.out.println("Produto: Azei");
-        case "Oleo"     -> System.out.println("Produto: Oleo");
-        case "Arroz"    -> System.out.println("Produto: Arroz");
-        case "Açucar"   -> System.out.println("Produto: Açucar");
-        case "Farinhas" -> System.out.println("Produto: Farinhas");
+            case "Azeite"   -> System.out.println("Produto: Azeite");
+            case "Oleo"     -> System.out.println("Produto: Oleo");
+            case "Arroz"    -> System.out.println("Produto: Arroz");
+            case "Açucar"   -> System.out.println("Produto: Açucar");
+            case "Farinhas" -> System.out.println("Produto: Farinhas");
+            default         -> System.out.println("Dados Invalidos!");
+
         }
+
     }
 }
