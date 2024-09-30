821ec13a007ad3c9d5c76e92bd5c32c8106cd8c8
Edvaldo Ferreira <ed.ferreiraoliveira@gmail.com>
2024-09-30 21:43:17 +0100

    Implementando Program

--- src/com/devsuperpoo/interfaces2/application/Program.java
+++ src/com/devsuperpoo/interfaces2/application/Program.java
@@ -1,5 +1,6 @@
 package com.devsuperpoo.interfaces2.application;
 
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Scanner;
@@ -11,9 +12,16 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        DateTimeFormatter fnt = DateTimeFormatter.ofPattern("dd/MM/yyyy : HH:mm");
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy : HH:mm");
 
         System.out.println("Entre os dados do contrato: ");
+
+        System.out.print("Numero: ");
+        int number = sc.nextInt();
+        System.out.print("Data (dd/MM/yyyy): ");
+        LocalDate date = LocalDate.parse(sc.next(), fmt);
+        System.out.print("Valor do contrato: ");
+        double totalValue = sc.nextDouble();
 
 
     }
