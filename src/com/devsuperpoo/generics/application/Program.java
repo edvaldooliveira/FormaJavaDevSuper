1a6b54f3bd3561c9f7c061e7a03dd6f78dabeae8
Edvaldo Ferreira <ed.ferreiraoliveira@gmail.com>
2024-10-03 22:59:31 +0100

    Generics under construction

--- src/com/devsuperpoo/generics/application/Program.java
+++ src/com/devsuperpoo/generics/application/Program.java
@@ -7,6 +7,9 @@ public class Program {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        System.out.print("How many values? ");
+       
+
 
 
     }
--- src/com/devsuperpoo/generics/service/PrintService.java
+++ src/com/devsuperpoo/generics/service/PrintService.java
@@ -10,4 +10,8 @@ public class PrintService <T> {
     public void addValue(T value) {
         list.add(value);
     }
+
+
+
+
 }
