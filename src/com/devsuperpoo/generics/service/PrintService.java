17a752775c98074b22849da623dff604d872dbc2
Edvaldo Ferreira <ed.ferreiraoliveira@gmail.com>
2024-10-03 22:42:31 +0100

    Home generics1

---
+++ src/com/devsuperpoo/generics/service/PrintService.java
@@ -0,0 +1,13 @@
+package com.devsuperpoo.generics.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrintService <T> {
+
+    private List<T> list = new ArrayList<>();
+
+    public void addValue(T value) {
+        list.add(value);
+    }
+}
