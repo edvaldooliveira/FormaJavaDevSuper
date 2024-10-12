a9e7e2f38c4ca3dd951742833dda2907e33370be
Edvaldo Ferreira <ed.ferreiraoliveira@gmail.com>
2024-10-12 17:22:32 +0100

    Comparator objeto de expressão lambda

--- src/com/devsuperpoo/comparator/application/Program.java
+++ src/com/devsuperpoo/comparator/application/Program.java
@@ -3,7 +3,6 @@ package com.devsuperpoo.comparator.application;
 import com.devsuperpoo.comparator.entities.Product;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 public class Program {
@@ -16,16 +15,8 @@ public class Program {
         list.add(new Product("Notebook", 1200.00));
         list.add(new Product("Tablet", 450.00));
 
-        //list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
-
-        Comparator<Product> comp = new Comparator<Product>() {
-            @Override
-            public int compare(Product p1, Product p2) {
-                return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-            }
-        };
-
-        list.sort(comp);
+        
+        list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
 
         for (Product p : list) {
             System.out.println(p);
