d6a0c090e3707728fc3d7a3ad0cc7267827d4881
Edvaldo Ferreira <ed.ferreiraoliveira@gmail.com>
2024-10-12 15:47:08 +0100

    Adicionado produto a lista

---
+++ src/com/devsuperpoo/comparator/application/Program.java
@@ -0,0 +1,17 @@
+package com.devsuperpoo.comparator.application;
+
+import com.devsuperpoo.comparator.entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+    public static void main(String[] args) {
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Notebook", 1200.00));
+        list.add(new Product("Tablet", 450.00));
+
+    }
+}
