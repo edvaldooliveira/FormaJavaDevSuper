b1339accb5febed33b9da80c93584e738f52c254
Edvaldo Ferreira <ed.ferreiraoliveira@gmail.com>
2024-10-13 09:40:54 +0100

    implementado expression lambda

---
+++ src/com/devsuperpoo/lambda/application/Program.java
@@ -0,0 +1,28 @@
+package com.devsuperpoo.lambda.application;
+
+import com.devsuperpoo.lambda.entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
+
+        list.add(new Product("Notebook Dell", 99.0));
+
+        double min = 100.0;
+
+        list.removeIf(p -> p.getPrice() >= min);
+
+        for (Product p : list) {
+            System.out.println(p);
+        }
+    }
+}
