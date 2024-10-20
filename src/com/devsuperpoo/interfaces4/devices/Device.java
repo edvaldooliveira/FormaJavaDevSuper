baa2c2180d4550fa1d4bae1778c660196605fc74
Edvaldo Ferreira <ed.ferreiraoliveira@gmail.com>
2024-10-20 11:35:13 +0100

    implementar mais de uma interface

---
+++ src/com/devsuperpoo/interfaces4/devices/Device.java
@@ -0,0 +1,20 @@
+package com.devsuperpoo.interfaces4.devices;
+
+public abstract class Device {
+
+    public String serialNumber;
+
+    public Device(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public abstract void processDoc(String doc);
+}
---
+++ src/com/devsuperpoo/interfaces4/devices/Printer.java
@@ -0,0 +1,6 @@
+package com.devsuperpoo.interfaces4.devices;
+
+public interface Printer {
+
+    void print(String doc);
+}
